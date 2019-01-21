ac982cfec6d2ee94abda3f1c2e5215b1c0f13c67
Joel <joelpalsson@hotmail.com>
2019-01-21 17:28:37 +0100

    Add code for reading the room temperature

--- SmartHome.ino
+++ SmartHome.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+const double tempSensorPin = 0;
+const double maxSensorInput = 1024;
+const double maxVoltage = 5;
+const double scaleFactor = 100;
+const double offset = 50;
+int sensorInput;
+double temp;
 
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  sensorInput = analogRead(tempSensorPin);
+  temp = sensorInput / maxSensorInput * maxVoltage * scaleFactor - offset;
+  Serial.println(temp);
+  delay(3000);
 }
